57fa01e24e238692186db945308fb98cf5c2d45b
LOKESH M <115631279+loky0612@users.noreply.github.com>
2024-02-28 07:42:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use('/',require('./routes/authRoutes'));
 
 app.listen(8001,() => {
-    console.log("Server Connected at port 8000");
+    console.log("Server Connected at port 8001");
     db.connect((err) => {
         if (err) throw err;
         console.log("DB connected sucessfully");
